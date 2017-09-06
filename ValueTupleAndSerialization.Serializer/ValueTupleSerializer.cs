be051f6c0d4edb660b232f8ae776b3214948c547
Jason Bock <jason@jasonbock.net>
2017-09-06 14:43:01 -0500

    Minor changes

--- ValueTupleAndSerialization.Serializer/ValueTupleSerializer.cs
+++ ValueTupleAndSerialization.Serializer/ValueTupleSerializer.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization;
 using System.Runtime.Serialization.Formatters.Binary;
 
 namespace ValueTupleAndSerialization.Serializer
@@ -16,6 +18,25 @@ namespace ValueTupleAndSerialization.Serializer
 				stream.Position = 0;
 				return ((int, int))formatter.Deserialize(stream);
 			}
+			//	var isSerializable = value.GetType().IsSerializable;
+
+			//Console.Out.WriteLine(isSerializable);
+
+			//if(isSerializable)
+			//{
+			//	var formatter = new BinaryFormatter();
+				
+			//	using (var stream = new MemoryStream())
+			//	{
+			//		formatter.Serialize(stream, value);
+			//		stream.Position = 0;
+			//		return ((int, int))formatter.Deserialize(stream);
+			//	}
+			//}
+			//else
+			//{
+			//	throw new SerializationException($"Cannot serialize {value.GetType().Name}");
+			//}
 		}
 	}
 }
